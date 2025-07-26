66a4eb81e6e4da1ff1f445820b36027e8f36f5a7
DanielV593 <juan.vasquez03@epn.edu.ec>
2025-07-26 12:32:56 -0500

    Archivos para QT

---
+++ Proyecto_QT_Administrador_De_Proyectos/build/Desktop_Qt_6_9_1/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_Proyecto_QT_Administrador_De_Proyectos_FILE C:/Users/User/Documents/Proyecto/Proyecto_QT_Administrador_De_Proyectos/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/Proyecto_QT_Administrador_De_Proyectos.exe)
+set(__QT_DEPLOY_TARGET_Proyecto_QT_Administrador_De_Proyectos_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_Proyecto_QT_Administrador_De_Proyectos_RUNTIME_DLLS C:/Qt/6.9.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Core.dll)
